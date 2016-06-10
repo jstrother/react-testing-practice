e4f2f016682d5e49d704219b7d6d50a57c15ffea
Jim Strother <jim.strother@gmail.com>
2016-06-09 19:52:13 -0700

    adding test code

---
+++ test/test-image.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+const TestUtils = require('react-addons-test-utils');
+const should = require('chai').should();
+
+const Image = require('../image');
+
+describe('Image component', function() {
+	it('Renders the image and description', function() {
+		
+	});
+});
