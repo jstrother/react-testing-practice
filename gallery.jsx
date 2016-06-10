0c616e149a976f379183f272309f936452db251d
Jim Strother <jim.strother@gmail.com>
2016-06-09 19:45:46 -0700

    first commit

---
+++ gallery.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+const Image = require('./image');
+
+var Gallery = function(props) {
+	var images = props.images.map(function(image, index) {
+		return <Image url={image.url} description={image.description} key={index} />
+	});
+
+	return (
+		<div className="gallery">
+			{images}
+		</div>
+	);
+};
+
+module.exports = Gallery;
---
+++ image.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+var Image = function(props) {
+	return (
+		<div className="gallery-image">
+			<img src={props.url} alt={props.description} />
+			<p>{props.description}</p>
+	);
+};
+
+module.exports = Image;
